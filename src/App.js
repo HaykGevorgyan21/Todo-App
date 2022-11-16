60f835dda9ea2c2a444144b4f4bef0785bd77464
Hayk Gevorgyan <109313231+HaykGevorgyan21@users.noreply.github.com>
2022-11-16 23:52:41 +0400

    change style todo_Home

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import "./App.css";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
-import TodoList from "./Pages/Home";
-import {Navigates} from "./Navigate";
+// import TodoList from "./Pages/Home";
+import { Navigates } from "./Navigate";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Profile from "./Pages/Profile";
 import TodoBody from "./Components/TodoBody/TodoBody";
 import ProtectRoutes from "./API/ProtectRoutes";
+// import Todo from "./Pages/todo";
+import TodoList from "./Pages/Home/index";
 import { Burgermenu } from "./Navigate";
-
 
 function App() {
   return (
     <div className="App">
       <Navigates />
-        <Routes>
+      <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route element={<ProtectRoutes />}>
-        <Route path="/home" element={<TodoBody />} />
-        <Route path="/profile" element={<Profile />} />
+          <Route path="/home" element={<TodoList />} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
-        <Route path="*" element={<Navigate  to={'/register'} replace/>} />
+        <Route path="*" element={<Navigate to={"/register"} replace />} />
       </Routes>
     </div>
   );
